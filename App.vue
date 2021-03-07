e78a9fd8fd31441da531bf7a7ae641862cbe2b5f
ethjvx <2037999319@qq.com>
2021-03-08 00:58:59 +0800

    23

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      xxx
+      {{ msg }}
+      <p>llal</p>
+      <base-son />
+  </div>
+  
+</template>
+
+<script>
+import baseSon from './Son';
+export default {
+    components: {
+        baseSon
+    },
+    data(){
+        return {
+            msg:"hello word"
+        }
+    }
+}
+</script>
+
+<style>
+    div {
+        background-color: red;
+    }
+</style>
---
+++ Son.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    this is a son
+    <p>hahaah is a p</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+
+};
+</script>
+
+<style scoped>
+
+    p {
+        background-color: green;
+    }
+</style>
