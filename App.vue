9cbc3541c089bfa485c03143e7453c5dc7990839
ethjvx <2037999319@qq.com>
2021-03-09 16:09:28 +0800

    24

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <!-- <date-picker :date="date" @choose-date="chooseDate" /> -->
+    <date-picker v-model="date" />
+  </div>
+</template>
+
+<script>
+import DatePicker from "./DatePicker";
+
+export default {
+  components: {
+    DatePicker
+  },
+  data() {
+    return {
+      date: new Date()
+    };
+  }
+  // methods: {
+  //   chooseDate(date1) {
+  //     console.log(this.date)
+  //     this.date = date1;
+  //   }
+  // }
+};
+</script>
+
+<style scoped>
+</style>
